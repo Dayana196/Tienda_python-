242d11908bef189640147d692d9c5e8e26715c2e
Dayana Barbosa <dayanita1204bb@gmail.com>
2025-05-23 12:56:21 -0500

    clase 23 de mayo -python

---
+++ shop.py
@@ -0,0 +1,78 @@
+productos = ["pan","harina","chocorramo","gala","coca-cola zero"]
+precios = [2000, 3000, 4500,4800,4500]
+
+nombretienda = "Mi tienda!💰"
+print (f"Bienvenidos a {nombretienda}")
+print (f"productos disponibles: {len(productos)}")
+
+carrito=[]
+cantidad=[]
+total=0 
+
+
+
+
+
+
+
+
+
+while True: 
+    
+    print(lastmenu) 
+    opcion =input ("seleccione una opcion: ") 
+    if opcion =="1":
+        for posicion in range (5):
+            print ("-----------------")
+            print (f"{posicion+1}.{productos[posicion]} $ {precios[posicion]}")
+            print ("-----------------")
+        input ("continuar...")
+
+    elif opcion =="2":
+        print (f"productos disponibles: {len(productos)}")
+        for posicion in range (5):
+            print ("-----------------")
+            print (f"{posicion+1}.{productos[posicion]} $ {precios[posicion]}")
+        busqueda= int(input("ingrese la posicion del producto"))
+        if busqueda > 0 or busqueda >len (productos): 
+            print ("Sellecione un rango especifico valido!")
+            input ("continuar")
+            continue
+        cantidad_busqueda = int(input(f"ingrese la cantidad del producto{productos[buasqueda]}${precios[busqueda]}"))
+        carrito.append(productos[busqueda])
+        cantidad.append(cantidad_busqueda)
+        total-total + (cantidad_busqueda * precios[busqueda])
+
+
+    elif opcion =="3":
+        print("hasta pronto!") 
+        input ("continuar...")
+        break
+    else: 
+        print ("opcion no valida")
+        input ("continuar...")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
